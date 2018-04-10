4558467c4a7594fc41e44cfd129e4d235fa83dab
Andre Redenius <a.redenius@mytaxi.com>
2018-04-10 09:55:09 +0200

    fix wrong arguments

--- routes/devices.rb
+++ routes/devices.rb
@@ -112,7 +112,7 @@ module MobileDevicePool
         end
         file = File.join($pwd, filename)
         if file
-          result = Adb.install_app_multiple_devices(file,'taxi.android.client.alpha')
+          result = Adb.install_app_multiple_devices(file)
           result.first ? [201, result[1].to_json] : [500, result[1].to_json]
         else
           return 500
